52298fc98dca0cc034f0f3fd2038caeb58ef5784
root <root@ip-172-31-22-96.ap-southeast-1.compute.internal>
2024-12-05 10:22:54 +0000

    the install

---
+++ main1.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+    region = "ap-southeast-1"
+    
+}
+
+resource "aws_instance" "instance-1" {
+    ami = "ami-047126e50991d067b"
+    instance_type = "t2.medium"
+    security_groups = ["default"]
+    key_name = "project"
+    user_data = file("docker.sh")
+    tags = {
+        Name = "Microk8s"
+    }
+   root_block_device {
+     volume_size = 30
+     volume_type = "gp3"
+     delete_on_termination = true
+   }
+}
